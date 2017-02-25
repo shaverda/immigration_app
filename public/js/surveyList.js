6110665bcf0284dab10b4feefc1d732c13259f81
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2017-02-25 12:57:16 -0600

    uncommented survey-api-routes. List.handlebars now pulls in data from mysql

--- public/js/surveyList.js
+++ public/js/surveyList.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-
+	
 	function getPosts() {
 	  $.get("/api/surveyList", function(data) {
 	    console.log("Survey", data);
-	    survey = data;
+	    surveyList = data;
+	  }).done(function(){
+	  	initializeRows(surveyList);
 	  });
 	}
 
-	function initializeRows() {
-		$('.list').html('hello');
-	}
+	function initializeRows(list) {
+		console.log(list[0].surveyName);
+		$('.list').text(list[0].surveyName);
+		for (var i = 0; i < list.length; i++) {
+    		$('.list').append(list[i].surveyName);
+    		$('.list').append(' -- ');
+    	};
+
+    	//res.render('surveyList', {survey: surveyList});
+	};
+	getPosts();
 });
