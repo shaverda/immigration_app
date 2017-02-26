4f10f605ecb66114901c25dec554f6c47436d69d
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2017-02-26 17:54:24 -0600

    created seperate navbars for users/lawyers

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,11 +6,29 @@
 // =============================================================
 var path = require("path");
 var exphbs = require("express-handlebars");
+var api = require("./survey-api-routes");
+
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+  //0 user, 1 laywer, 2 test
+  var userType = 0;
+
+  switch(userType){
+    case 0:
+      app.engine('handlebars', exphbs({ defaultLayout: 'user' }));
+      break;
+
+    case 1:
+      app.engine('handlebars', exphbs({ defaultLayout: 'laywer'}));
+      break;
+
+    default:
+      app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+  };
+
   app.set("view engine", "handlebars");
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
@@ -22,7 +40,7 @@ module.exports = function(app) {
 
   //lawyers are sent to an list of surveys filled out forms
   app.get("/surveyList", function(req, res) {
-    res.render('surveyList');
+    res.render('surveyList', {});
   });
 
   //users are sent to thier survey
