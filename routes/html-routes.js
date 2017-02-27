453b97ad4156a670012b69f94bb7c074babe75a2
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2017-02-26 20:04:28 -0600

    Added logout button to lawyer/user.handlebar files, corrected html route, and removed the switch statement to direct the user the correct layout. They are now directed to user by default, and if their login type is lawyer, we take them to the surveyList and set the defaultLayout to lawyer there.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,27 +8,15 @@ var path = require("path");
 var exphbs = require("express-handlebars");
 var api = require("./survey-api-routes");
 
-
 // Routes
 // =============================================================
 module.exports = function(app) {
 
   //0 user, 1 laywer, 2 test
-  var userType = 0;
+  
 
-  switch(userType){
-    case 0:
-      app.engine('handlebars', exphbs({ defaultLayout: 'user' }));
-      break;
-
-    case 1:
-      app.engine('handlebars', exphbs({ defaultLayout: 'laywer'}));
-      break;
-
-    default:
-      app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-  };
-
+  app.engine('handlebars', exphbs({ defaultLayout: 'user' }));
+  console.log('user');
   app.set("view engine", "handlebars");
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
@@ -40,6 +28,8 @@ module.exports = function(app) {
 
   //lawyers are sent to an list of surveys filled out forms
   app.get("/surveyList", function(req, res) {
+    app.engine('handlebars', exphbs({ defaultLayout: 'lawyer'}))
+    app.set("view engine", "handlebars");
     res.render('surveyList', {});
   });
 
