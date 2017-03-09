64265e7c21355ee09f9f8969548ac9780940d7bd
Shaverda <shaverda@gmail.com>
2017-03-09 09:46:48 -0600

    fixed show/download pdf on show_survey

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,25 +1,47 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-// var image = document.getElementById('source');
-
 
 var imageObj = new Image();
 imageObj.onload = function() {
     context.drawImage(imageObj, 0, 0, 750, 975);
     context.font = "10pt Arial";
-    context.fillText($("#first_name").text(), 50, 175);
+    context.fillText($("#last_name").text(), 50, 175);
+    context.fillText($("#first_name").text(), 200, 175);
 };
 imageObj.src = "i485.png";
 
 
- function getMousePos(canvas, evt) {
+function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
     };
-  }
-  canvas.addEventListener('mousemove', function(evt) {
+}
+canvas.addEventListener('mousemove', function(evt) {
     var mousePos = getMousePos(canvas, evt);
     console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
-  }, false);
+}, false);
+
+download.addEventListener("click", function() {
+    // only jpeg is supported by jsPDF
+    var imgData = canvas.toDataURL("image/jpeg", 1.0);
+    var pdf = new jsPDF();
+
+    pdf.addImage(imgData, 'JPEG', 0, 0);
+    var download = document.getElementById('download');
+
+    pdf.save("download.pdf");
+}, false);
+
+$("#display_pdf").click(function() {
+  $("#canvas").css("display", "");
+  $(this).css("display", "none");
+  $("#hide_pdf").css("display", "");
+});
+
+$("#hide_pdf").click(function() {
+  $("#canvas").css("display", "none");
+  $(this).css("display", "none");
+  $("#display_pdf").css("display", "");
+})
